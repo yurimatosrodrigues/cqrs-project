8159d6279394ec3ba913085e4c40d7da77a47e98
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2025-10-31 13:07:35 -0300

    feat: implement command create member

---
+++ CleanArch.Application/Members/Commands/CreateMemberCommand.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Entities;
+using MediatR;
+
+namespace CleanArch.Application.Members.Commands
+{
+    public class CreateMemberCommand : IRequest<Member>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ CleanArch.Application/Members/Handlers/CreateMemberCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Members.Commands;
+using CleanArch.Domain.Abstractions;
+using CleanArch.Domain.Entities;
+using MediatR;
+
+namespace CleanArch.Application.Members.Handlers
+{
+    public class CreateMemberCommandHandler : IRequestHandler<CreateMemberCommand, Member>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateMemberCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Member> Handle(CreateMemberCommand request, CancellationToken cancellationToken)
+        {
+            var newMember = new Member(request.FirstName, request.LastName, 
+                request.Gender, request.Email, request.IsActive);
+
+            await _unitOfWork.MemberRepository.AddMemberAsync(newMember);
+            await _unitOfWork.CommitAsync();
+            return newMember;
+        }
+    }
+}
